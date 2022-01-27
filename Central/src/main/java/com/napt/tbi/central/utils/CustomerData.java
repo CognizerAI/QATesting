89e16f3de2dd9d9f6fe6a23e6c3b40e6bc101446
Swapnil Vaidya - Cognizer Account <swapnil.vaidya@cognizer.ai>
2022-01-27 07:33:25 +0530

    added on 27th Jan_5
    added on 27th Jan_5

--- Central/src/main/java/com/napt/tbi/central/utils/CustomerData.java
+++ Central/src/main/java/com/napt/tbi/central/utils/CustomerData.java
@@ -18,6 +18,7 @@ public class CustomerData {
             //adde on 27th Jan_2
             //adde on 27th Jan_3
             //adde on 27th Jan_4
+            //added on 27th Jan_5
             sb.append(AlphaNumericString.charAt(index));
         }
         return sb.toString();
